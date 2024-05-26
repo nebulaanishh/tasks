abc947c4d35eba21e2d6991a6f00f6a07a8a6e4e
Balaram Neupane <balaram@ramailo.tech>
2024-05-26 21:03:17 +0545

    fix: minor fix to include id in response

--- backend/recipe/serializers/recipe.py
+++ backend/recipe/serializers/recipe.py
@@ -4,5 +4,5 @@ from recipe.models import Recipe
 class RecipeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recipe
-        fields = ['name', 'instructions', 'cook_time_minutes', 'servings', 'difficulty', 'cuisine', 'calories_per_serving', 'image', 'rating', 'ingredients', 'tags']   
+        fields = ['id','name', 'instructions', 'cook_time_minutes', 'servings', 'difficulty', 'cuisine', 'calories_per_serving', 'image', 'rating', 'ingredients', 'tags']   
 
