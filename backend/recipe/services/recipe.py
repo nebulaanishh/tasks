5e878893aa47ddafa30dceaceda7b15ac487ae51
Balaram Neupane <balaram@ramailo.tech>
2024-05-27 20:06:41 +0545

    fix: fixed serializer validity check logic

--- backend/recipe/services/recipe.py
+++ backend/recipe/services/recipe.py
@@ -10,10 +10,9 @@ from rest_framework import status
 class RecipeService():
 
     def get_all_recipes(self):
-        """ """
         recipes = Recipe.objects.all()
         serializer = RecipeSerializer(recipes, many=True)
-        if not serializer:
+        if not serializer.is_valid():
             return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
@@ -35,7 +34,7 @@ class RecipeService():
     def get_recipe_detail(self, pk):
         recipe = self.get_object(pk)
         serializer = RecipeSerializer(recipe)
-        if not serializer:
+        if not serializer.is_valid():
             return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
         return Response(serializer.data, status=status.HTTP_200_OK)
     
@@ -44,7 +43,7 @@ class RecipeService():
         serializer = RecipeSerializer(recipe, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete_recipe(self, pk):
@@ -53,6 +52,3 @@ class RecipeService():
             recipe.delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
         return Response(status=status.HTTP_404_NOT_FOUND)
-
-
-
