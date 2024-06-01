67bfd90daa09f5ce5f671ccb0c8fc990bac99859
Balaram Neupane <balaram@ramailo.tech>
2024-06-01 22:13:03 +0545

    refactor: added exceptions & logging with test cases for proper error handling

--- backend/recipe/services/recipe.py
+++ backend/recipe/services/recipe.py
@@ -2,6 +2,7 @@ from recipe.models import Recipe
 from recipe.accessors.recipe import RecipeAccessor
 from recipe.serializers.recipe import RecipeSerializer
 from recipe.helpers.logger import configure_logging
+from recipe.builders.response_builder import ResponseBuilder
 
 from django.http import Http404
 from django.db import DatabaseError
@@ -10,38 +11,83 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.exceptions import ValidationError
 
+
 logger = configure_logging()
 
 
 class RecipeService:
 
     def get_all_recipes(self):
+        response_builder = ResponseBuilder()
         logger.info("FETCHING all recipes from database.")
         try:
             recipes = Recipe.objects.all()
             serializer = RecipeSerializer(recipes, many=True)
             logger.info(f"FETCHED {len(recipes)} recipes from database")
-            return Response(serializer.data, status=status.HTTP_200_OK)
+            return (
+                response_builder.result_object(serializer.data)
+                .success()
+                .ok_200()
+                .get_response()
+            )
         except DatabaseError as e:
             logger.error("HTTP 500 Internal Server Error")
-            return Response(
-                {"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            return (
+                response_builder.result_object(
+                    {"error": f"Database error occured: {e}"}
+                )
+                .fail()
+                .internal_error_500()
+                .message("Internal Error")
+                .get_response()
+            )
+        except Exception as e:
+            logger.error(f"Exception Occured: {e}")
+            return (
+                response_builder.result_object({"error": f"Exception Occured: {e}"})
+                .fail()
+                .bad_request_400()
+                .get_response()
             )
 
     def create_new_recipe(self, data):
         logger.info("CREATING new recipe")
+        response_builder = ResponseBuilder()
         try:
             serializer = RecipeSerializer(data=data)
             if serializer.is_valid():
                 serializer.save()
                 logger.info("CREATED Recipe successfully.")
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
-            logger.info("CREATE a new recipe FAILED. Validation errors occured")
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+                return (
+                    response_builder.result_object(serializer.data)
+                    .success()
+                    .accepted_202()
+                    .get_response()
+                )
+            logger.error("Validation Error in Create Recipe")
+            return (
+                response_builder.result_object({"error": f"{serializer.errors}"})
+                .fail()
+                .bad_request_400()
+                .get_response()
+            )
+
         except DatabaseError as e:
             logger.error("HTTP 500 Internal Server Error")
-            return Response(
-                {"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            return (
+                response_builder.result_object({"error": f"Database Error: {e}"})
+                .fail()
+                .internal_error_500()
+                .get_response()
+            )
+
+        except Exception as e:
+            logger.error(f"Exception Occured: {e}")
+            return (
+                response_builder.result_object({"error": f"Exception Occured: {e}"})
+                .fail()
+                .internal_error_500()
+                .get_response()
             )
 
     def get_object(self, pk):
@@ -53,43 +99,124 @@ class RecipeService:
 
     def get_recipe_detail(self, pk):
         logger.info("FETCHING recipe detail by id")
+        response_builder = ResponseBuilder()
         try:
             recipe = self.get_object(pk)
             serializer = RecipeSerializer(recipe)
             logger.info(f"FETCHED details for recipe with id {pk}")
-            return Response(serializer.data, status=status.HTTP_200_OK)
+            return (
+                response_builder.result_object(serializer.data)
+                .success()
+                .ok_200()
+                .get_response()
+            )
         except DatabaseError as e:
             logger.error("HTTP 500 Internal Server Error")
-            return Response(
-                {"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            return (
+                response_builder.result_object({"error": f"Database Error: {e}"})
+                .fail()
+                .internal_error_500()
+                .get_response()
+            )
+        except Http404 as e:
+            logger.error("Object not found")
+            return (
+                response_builder.result_object({"error": "Resource not found"})
+                .fail()
+                .not_found_404()
+                .get_response()
+            )
+        except Exception as e:
+            logger.error(f"Exception Occured: {e}")
+            return (
+                response_builder.result_object({"error": f"Exception Occured: {e}"})
+                .fail()
+                .internal_error_500()
+                .get_response()
             )
 
     def update_recipe_detail(self, request, pk):
         logger.info(f"UPDATING recipe with id: {pk}..")
+        response_builder = ResponseBuilder()
         try:
             recipe = self.get_object(pk)
             serializer = RecipeSerializer(recipe, data=request.data, partial=True)
             if serializer.is_valid():
                 serializer.save()
                 logger.info(f"UPDATE recipe with id: {pk} successfully")
-                return Response(serializer.data, status=status.HTTP_200_OK)
+                return (
+                    response_builder.result_object(serializer.data)
+                    .success()
+                    .ok_200()
+                    .get_response()
+                )
             logger.error(f"UPDATE recipe with id: {pk} Failed: Validation Errors")
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return (
+                response_builder.result_object({"error": serializer.errors})
+                .fail()
+                .bad_request_400()
+                .get_response()
+            )
         except DatabaseError as e:
             logger.error("HTTP 500 Internal Server Error")
-            return Response(
-                {"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            return (
+                response_builder.result_object({"error": f"Database Error: {e}"})
+                .fail()
+                .internal_error_500()
+                .get_response()
+            )
+        except Http404 as e:
+            logger.error("Object not found")
+            return (
+                response_builder.result_object({"error": "Resource not found"})
+                .fail()
+                .not_found_404()
+                .get_response()
+            )
+        except Exception as e:
+            logger.error(f"Exception Occured: {e}")
+            return (
+                response_builder.result_object({"error": f"Exception Occured: {e}"})
+                .fail()
+                .internal_error_500()
+                .get_response()
             )
 
     def delete_recipe(self, pk):
         logger.info(f"DELETING recipe with id: {pk}")
+        response_builder = ResponseBuilder()
         try:
             recipe = self.get_object(pk)
             recipe.delete()
-            logger.info(f"DELETED Recipe with id: {pk}successfully.")
-            return Response(status=status.HTTP_204_NO_CONTENT)
+            logger.info(f"DELETED Recipe with id: {pk} successfully.")
+            return (
+                response_builder.message("Deleted Successfully")
+                .success()
+                .accepted_202()
+                .get_response()
+            )
         except DatabaseError as e:
             logger.error("HTTP 500 Internal Server Error")
-            return Response(
-                {"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            return (
+                response_builder.result_object({"error": f"Database Error: {e}"})
+                .fail()
+                .internal_error_500()
+                .get_response()
+            )
+        except Http404 as e:
+            logger.error("Object not found")
+            return (
+                response_builder.result_object({"error": "Resource not found"})
+                .fail()
+                .not_found_404()
+                .get_response()
+            )
+
+        except Exception as e:
+            logger.error(f"Exception Occured: {e}")
+            return (
+                response_builder.result_object({"error": f"Exception Occured: {e}"})
+                .fail()
+                .internal_error_500()
+                .get_response()
             )
--- backend/recipe/tests/test_service.py
+++ backend/recipe/tests/test_service.py
@@ -6,50 +6,74 @@ from rest_framework.parsers import JSONParser
 from recipe.models.recipe import Recipe
 from recipe.services.recipe import RecipeService
 from recipe.tests.test_setup import TestRecipeSetup
+from recipe.builders.response_builder import ResponseBuilder
+
+response_builder = ResponseBuilder()
+
 
 class TestRecipeService(TestRecipeSetup):
-    
+
     def setup(self):
         super().setup()
         self.factory = APIRequestFactory()
         self.service = RecipeService()
-    
-    def test_get_all_recipes(self):
+
+    def test_get_all_recipes_success(self):
         self.setup()
-        request = self.factory.get('/recipes/')
         response = self.service.get_all_recipes()
         print(response)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 1)
-    
+        self.assertEqual(response.data["status-code"], 1)
+        self.assertEqual(
+            response.data.get("data")[0].get("name", ""), self.recipe.get("name")
+        )
+        self.assertEqual(len(response.data["data"]), 1)
+
     def test_create_new_recipe(self):
         self.setup()
-        request = self.factory.post('/recipes/', self.recipe, format='json')
         response = self.service.create_new_recipe(self.recipe)
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(response.data['name'], self.recipe.get("name", ""))
-    
+        self.assertEqual(response.status_code, 202)
+        self.assertEqual(response.data["status-code"], 1)
+        self.assertEqual(response.data.get("data").get("name"), self.recipe.get("name"))
+
+    def test_create_new_recipe_validation_error(self):
+        self.setup()
+        self.recipe.pop("instructions")
+        response = self.service.create_new_recipe(self.recipe)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.data["status-code"], -1)
+
     def test_get_recipe_detail(self):
         self.setup()
-        request = self.factory.get(f"/recipes/{self.recipe_object.id}")
         response = self.service.get_recipe_detail(self.recipe_object.id)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['name'], self.recipe.get('name'))
+        self.assertEqual(response.data["status-code"], 1)
+        self.assertEqual(response.data["data"]["name"], self.recipe.get("name"))
+
+    def test_get_recipe_detail_404(self):
+        self.setup()
+        response = self.service.get_recipe_detail(11)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(response.data["status-code"], -1)
 
     def test_update_recipe_detail(self):
         self.setup()
-        update_data = {"name" : "updated recipe"}
-        factory_request = self.factory.patch(f"/recipes/{self.recipe_object.pk}/", update_data ,format='json')
+        update_data = {"name": "updated recipe"}
+        factory_request = self.factory.put(
+            f"/recipes/{self.recipe_object.pk}/", update_data, format="json"
+        )
 
         drf_request = Request(factory_request, parsers=[JSONParser()])
         response = self.service.update_recipe_detail(drf_request, self.recipe_object.pk)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['name'], update_data.get("name"))
+        self.assertEqual(response.data["status-code"], 1)
+        self.assertEqual(response.data["data"]["name"], update_data.get("name"))
 
     def test_delete_recipe(self):
         self.setup()
         request = self.factory.delete(f"/recipes/{self.recipe_object.pk}/")
         response = self.service.delete_recipe(self.recipe_object.pk)
-        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(response.status_code, 202)
+        self.assertEqual(response.data["status-code"], 1)
         with self.assertRaises(Recipe.DoesNotExist):
             Recipe.objects.get(pk=self.recipe_object.pk)
